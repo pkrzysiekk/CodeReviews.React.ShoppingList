f276cc00e51a8c6cf81b0aaf736bae5b37e38165
pkrzysiek <pkrzysiek24@gmail.com>
2025-07-09 16:40:55 +0200

    Add CORS to Program.cs

--- ShoppingList.pkrzysiekk/ShoppingListAPI/ShoppingListAPI/Program.cs
+++ ShoppingList.pkrzysiekk/ShoppingListAPI/ShoppingListAPI/Program.cs
@@ -5,17 +5,26 @@ using ShoppingListAPI.Repository;
 using ShoppingListAPI.Service;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("http://localhost:5173");
+                      });
+});
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<ShoppingContext>(options=>options.UseSqlite("Data Source=ShoppingList.db"));
+builder.Services.AddDbContext<ShoppingContext>(options =>
+    options.UseSqlite("Data Source=ShoppingList.db")
+);
 builder.Services.AddScoped<IRepository<ShoppingItem>, ShoppingRepository>();
 builder.Services.AddScoped<IShoppingService, ShoppingService>();
-
-
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
@@ -25,14 +34,12 @@ using (var scope = app.Services.CreateScope())
     if (!db.ShoppingItems.Any())
         db.Initialize();
 }
-
+app.UseCors(MyAllowSpecificOrigins);
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
